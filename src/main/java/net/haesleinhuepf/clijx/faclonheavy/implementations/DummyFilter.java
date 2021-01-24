42e55651d5c9f942eb04f19137013561dbb84462
haesleinhuepf <robert_haase@gmx.de>
2021-01-24 15:33:10 +0100

    added time measurements

--- src/main/java/net/haesleinhuepf/clijx/faclonheavy/implementations/DummyFilter.java
+++ src/main/java/net/haesleinhuepf/clijx/faclonheavy/implementations/DummyFilter.java
@@ -3,6 +3,7 @@ package net.haesleinhuepf.clijx.faclonheavy.implementations;
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clijx.faclonheavy.AbstractTileProcessor;
 
+import java.util.Arrays;
 import java.util.function.BiConsumer;
 
 /**
@@ -14,6 +15,9 @@ public class DummyFilter extends AbstractTileProcessor {
 
     @Override
     public void accept(ClearCLBuffer input, ClearCLBuffer output) {
+        System.out.println("Start processing on " + clijx.getGPUName() + " image dimensions " + Arrays.toString(input.getDimensions()));
+        long start_time = System.nanoTime();
+
         // allocated temporary memory
         ClearCLBuffer temp = clijx.create(input);
 
@@ -23,5 +27,7 @@ public class DummyFilter extends AbstractTileProcessor {
 
         // clean up
         temp.close();
+        long duration_ms = (System.nanoTime() - start_time) / 1000000;
+        System.out.println("Finished processing on " + clijx.getGPUName() + " after " + duration_ms + " ms");
     }
 }
